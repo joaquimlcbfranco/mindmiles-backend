8955b0a4ff26c482d711d9cef860dcca396bbd28
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-07-18 11:06:40 +0100

    feat: Create initial user controller, getUsers() method

---
+++ src/main/java/io/github/joaquimlcbfranco/mindmiles_backend/controller/UserController.java
@@ -0,0 +1,27 @@
+package io.github.joaquimlcbfranco.mindmiles_backend.controller;
+
+import io.github.joaquimlcbfranco.mindmiles_backend.entity.User;
+import io.github.joaquimlcbfranco.mindmiles_backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return this.userService.findAll();
+    }
+}
