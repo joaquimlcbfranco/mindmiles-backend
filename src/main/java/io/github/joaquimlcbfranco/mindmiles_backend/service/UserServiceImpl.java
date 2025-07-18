0d686a25856c0811d314578af8df653b09be7535
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-07-18 17:24:44 +0100

    feat: Implement methods from UserService interface for updating and deleting users

--- src/main/java/io/github/joaquimlcbfranco/mindmiles_backend/service/UserServiceImpl.java
+++ src/main/java/io/github/joaquimlcbfranco/mindmiles_backend/service/UserServiceImpl.java
@@ -1,12 +1,18 @@
 package io.github.joaquimlcbfranco.mindmiles_backend.service;
 
 import io.github.joaquimlcbfranco.mindmiles_backend.entity.User;
+import io.github.joaquimlcbfranco.mindmiles_backend.exception.InvalidParametersException;
+import io.github.joaquimlcbfranco.mindmiles_backend.exception.UserAlreadyExistsException;
+import io.github.joaquimlcbfranco.mindmiles_backend.exception.UserNotFoundException;
 import io.github.joaquimlcbfranco.mindmiles_backend.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
@@ -30,7 +36,18 @@ public class UserServiceImpl implements UserService {
             return user;
         }
 
-        return null;
+        throw new UserNotFoundException("User with username " + username + " not found");
+    }
+
+    public User getUserById(long id) {
+        Optional<User> foundUser = this.userRepository.findById(id);
+
+        if (foundUser.isPresent()) {
+            User user = foundUser.get();
+            return user;
+        }
+
+        throw new UserNotFoundException("User with id " + id + " not found");
     }
 
     @Override
@@ -41,10 +58,62 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public User addUser(User newUser) {
-        if (this.userRepository.findAll().contains(newUser.getUsername().toLowerCase())) {
-            return null;
+        if (newUser.getFirstName() == null || !newUser.getFirstName().matches("^[A-Za-z]+$")) {
+            throw new InvalidParametersException("First name must be alphabetic");
         }
 
+        if (newUser.getLastName() == null || !newUser.getLastName().matches("^[A-Za-z]+$")) {
+            throw new InvalidParametersException("Last name must be alphabetic");
+        }
+
+        if (newUser.getUsername() == null || !newUser.getUsername().matches("^[A-Za-z0-9_\\.]*$") || newUser.getUsername().length() < 8) {
+            throw new InvalidParametersException("Username must have more than 8 alphanumeric characters");
+        }
+
+        if (this.userRepository.findUserByUsername(newUser.getUsername()) != null) {
+            throw new UserAlreadyExistsException("Username is in use - " + newUser.getUsername());
+        }
+
+        User addedUser = this.userRepository.save(newUser);
+
+        return addedUser;
     }
+
+    @Override
+    @Transactional
+    public User updateUser(User newUser, long id) {
+        Optional<User> foundUser = this.userRepository.findById(id);
+
+        if (foundUser.isPresent()) {
+            User toUpdate = foundUser.get();
+            toUpdate.setFirstName(newUser.getFirstName());
+            toUpdate.setLastName(newUser.getLastName());
+            toUpdate.setUsername(newUser.getUsername());
+            toUpdate.setEmail(newUser.getEmail());
+            toUpdate.setDob(newUser.getDob());
+            toUpdate.setGender(newUser.getGender());
+
+            return toUpdate;
+        }
+
+        throw new UserNotFoundException("User with id " + id + "not found");
+    }
+
+    @Override
+    @Transactional
+    public String deleteUser(long id) {
+        Optional<User> foundUser = this.userRepository.findById(id);
+
+        if (foundUser.isPresent()) {
+            User toDelete = foundUser.get();
+            this.userRepository.delete(toDelete);
+            return "User with id " + toDelete.getId() + " deleted";
+        }
+
+        throw new UserNotFoundException("User with id " + id + "not found");
+    }
+
+
 }
