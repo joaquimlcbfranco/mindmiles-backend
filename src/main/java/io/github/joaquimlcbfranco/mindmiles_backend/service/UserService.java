e4644c29989e4d3c54df489261b6c75e3d6d2202
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-07-18 11:07:26 +0100

    feat: Create initial user service, implement findAll(), returns all users

---
+++ src/main/java/io/github/joaquimlcbfranco/mindmiles_backend/service/UserService.java
@@ -0,0 +1,24 @@
+package io.github.joaquimlcbfranco.mindmiles_backend.service;
+
+import io.github.joaquimlcbfranco.mindmiles_backend.entity.User;
+import io.github.joaquimlcbfranco.mindmiles_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
