c36def9d67d2e52e05f234165f89488d5eee89d4
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-07-18 12:19:01 +0100

    feat: Add query to filter/search users by first name, last name and username at the same time

--- src/main/java/io/github/joaquimlcbfranco/mindmiles_backend/repository/UserRepository.java
+++ src/main/java/io/github/joaquimlcbfranco/mindmiles_backend/repository/UserRepository.java
@@ -2,9 +2,18 @@ package io.github.joaquimlcbfranco.mindmiles_backend.repository;
 
 import io.github.joaquimlcbfranco.mindmiles_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    @Query(value = "SELECT * FROM users WHERE first_name = ?1 or last_name = ?1 or username = ?1", nativeQuery = true)
+    List<User> findUserBySearch(String searchWords);
 
 }
