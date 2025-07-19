f84db4fe8e178a2b203446a5bfea56733b940561
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-07-19 14:54:12 +0100

    feat: Add custom native queries to get activities by user ID and user ID + activity ID

--- src/main/java/io/github/joaquimlcbfranco/mindmiles_backend/repository/ActivityRepository.java
+++ src/main/java/io/github/joaquimlcbfranco/mindmiles_backend/repository/ActivityRepository.java
@@ -2,8 +2,16 @@ package io.github.joaquimlcbfranco.mindmiles_backend.repository;
 
 import io.github.joaquimlcbfranco.mindmiles_backend.entity.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
 
+    @Query(value = "SELECT * FROM activities WHERE user_id = ?1", nativeQuery = true)
+    List<Activity> findAllActivitiesByUserId(long userId);
 
+    @Query(value = "SELECT * FROM activities WHERE user_id = ?1 and id = ?2", nativeQuery = true)
+    Optional<Activity> findActivityByUserIdAndActivityId(long userId, long activityId);
 }
