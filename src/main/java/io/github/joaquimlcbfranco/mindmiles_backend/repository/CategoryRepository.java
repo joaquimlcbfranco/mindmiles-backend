0ddd933eb496e8861ec29eb0f9ec96696f0ab4a8
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-07-20 09:24:41 +0100

    feat: Define category repository based on JpaReposity

---
+++ src/main/java/io/github/joaquimlcbfranco/mindmiles_backend/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package io.github.joaquimlcbfranco.mindmiles_backend.repository;
+
+import io.github.joaquimlcbfranco.mindmiles_backend.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+}
