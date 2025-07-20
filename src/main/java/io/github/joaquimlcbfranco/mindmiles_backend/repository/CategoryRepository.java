a329d020b9f20fb626175644f6ada869e21c950d
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-07-20 10:09:54 +0100

    feat: Add custom native query to get categories by user_id

--- src/main/java/io/github/joaquimlcbfranco/mindmiles_backend/repository/CategoryRepository.java
+++ src/main/java/io/github/joaquimlcbfranco/mindmiles_backend/repository/CategoryRepository.java
@@ -2,8 +2,12 @@ package io.github.joaquimlcbfranco.mindmiles_backend.repository;
 
 import io.github.joaquimlcbfranco.mindmiles_backend.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-
+    @Query(value = "SELECT * FROM categories WHERE user_id = ?1", nativeQuery = true)
+    List<Category> findAllCategoriesByUserId(long id);
 }
