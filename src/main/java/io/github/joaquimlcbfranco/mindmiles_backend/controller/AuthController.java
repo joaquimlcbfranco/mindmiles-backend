4a0cc7902444a917d33fa530ab0432ba42ae0759
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-07-23 18:42:53 +0100

    feat: Create AuthController to handle registration and login requests

---
+++ src/main/java/io/github/joaquimlcbfranco/mindmiles_backend/controller/AuthController.java
@@ -0,0 +1,26 @@
+package io.github.joaquimlcbfranco.mindmiles_backend.controller;
+
+import io.github.joaquimlcbfranco.mindmiles_backend.entity.User;
+import io.github.joaquimlcbfranco.mindmiles_backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+
+    private UserService userService;
+
+    @Autowired
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User createUser(@RequestBody User user) {
+        return this.userService.addUser(user);
+    }
+}
