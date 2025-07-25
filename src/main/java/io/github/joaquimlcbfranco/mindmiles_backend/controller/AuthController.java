ed56b5f017d0bc77250ca2a9e81c33eb4067555a
joaquimlcbfranco <joaquimlcbfranco@gmail.com>
2025-07-25 12:28:14 +0100

    feat: Create post mapping to handle login requests, check for invalid parameters

--- src/main/java/io/github/joaquimlcbfranco/mindmiles_backend/controller/AuthController.java
+++ src/main/java/io/github/joaquimlcbfranco/mindmiles_backend/controller/AuthController.java
@@ -1,8 +1,15 @@
 package io.github.joaquimlcbfranco.mindmiles_backend.controller;
 
+import io.github.joaquimlcbfranco.mindmiles_backend.dto.LoginRequest;
 import io.github.joaquimlcbfranco.mindmiles_backend.entity.User;
+import io.github.joaquimlcbfranco.mindmiles_backend.exception.BadCredentialsException;
+import io.github.joaquimlcbfranco.mindmiles_backend.exception.UserNotFoundException;
 import io.github.joaquimlcbfranco.mindmiles_backend.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +29,23 @@ public class AuthController {
     @PostMapping("/register")
     public User createUser(@RequestBody User user) {
         return this.userService.addUser(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest, HttpSession session) {
+        try {
+            boolean isAuthenticated = this.userService.autheticate(loginRequest.getUsername(), loginRequest.getPassword());
+
+            if (isAuthenticated) {
+                session.setAttribute("user", loginRequest.getUsername());
+                return ResponseEntity.ok("Login was successful");
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+            }
+        } catch (UserNotFoundException | BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unknown error occurred" + e);
+        }
     }
 }
